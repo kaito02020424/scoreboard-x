f632473a818f6de9b40216bd6bf4f77d39793e56
kaito02020424 <kaito02020424@gmail.com>
2024-07-02 19:34:32 +0900

    fix: Player was decayed

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,13 @@
-import { NetworkIdentifier } from "bdsx/bds/networkidentifier";
-import { Player } from "bdsx/bds/player";
 import { ScoreboardX } from "..";
 import { events } from "bdsx/event";
+import { bedrockServer } from "bdsx/launcher";
 
-const players = new Map<NetworkIdentifier, Player>();
 
 const intrval = setInterval(() => {
-    const players_ = Array.from(players.entries());
-    for (let i = 0; i < players_.length; i++) {
-        const [netId, player] = players_[i];
+    for (const player of bedrockServer.level.getPlayers().filter(player => !player.isSimulated())) {
         ScoreboardX.sendTo(player);
     }
 }, ScoreboardX.getSpeed());
-
-events.playerJoin.on((ev) => {
-    players.set(ev.player.getNetworkIdentifier(), ev.player);
-});
-
-events.playerLeft.on((ev) => {
-    players.delete(ev.player.getNetworkIdentifier());
-});
 
 events.serverStop.on(() => {
     clearInterval(intrval);
